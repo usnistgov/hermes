3d05637993a664a2540ba6695bab6ee244307381
Camilo Velez <camilov61@gmail.com>
2022-10-17 09:26:44 -0400

    refactor: class IDA

--- hermes/ida/__init__.py
+++ hermes/ida/__init__.py
@@ -5,7 +5,7 @@ Created on Tue Sep 27 12:15:35 2022
 @author: Austin McDannald
 """
 
-class Intrinsic_data_analysis:
+class IDA:
     """Base level class for analyising the intrinsic properties of the data.
     All inputs are treated as features."""
     pass
