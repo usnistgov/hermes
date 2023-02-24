296b749f554fe5322c29995a9e5f52372ff2fac8
Camilo Velez <camilov61@gmail.com>
2023-02-24 13:34:08 -0500

    fix: fixed __init__

--- hermes/__init__.py
+++ hermes/__init__.py
@@ -1,14 +1,18 @@
 """Top-level package for Hermes Python."""
 
-from hermes.distance import BaseDistance, EuclidianDistance, CosineDistance, PNormDistance
+from hermes.distance import (
+    BaseDistance,
+    EuclidianDistance,
+    CosineDistance,
+    PNormDistance,
+)
 
 __author__ = """Austin McDannald, Brian DeCost, Camilo Velez"""
 __email__ = "camilo.velezramirez@nist.gov"
 __version__ = "0.1.0"
 __all__ = [
-        "BaseDistance",
-        "EuclidianDistance",
-        "CosineDistance",
-        "PNormDistance",
-        "compute_distance",
-        ]
+    "BaseDistance",
+    "EuclidianDistance",
+    "CosineDistance",
+    "PNormDistance",
+]
