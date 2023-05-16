45216f2a9623623e3cdbf53ce2913aa1165e332a
Austin McDannald <68232794+austinmcdannald@users.noreply.github.com>
2023-05-16 16:02:47 -0400

    updating imports in init

--- hermes/__init__.py
+++ hermes/__init__.py
@@ -17,14 +17,33 @@
 #     ContiguousCluster,
 #     ContiguousFixedKClustering,
 # )
+from . import archive
+from . import base
+from . import classification
+from . import clustering
 from . import distance
+from . import instruments
+from . import pipelines
+from . import schemas
+from . import similarity
+from . import utils
+
 
 
 __author__ = """Austin McDannald, Brian DeCost, Camilo Velez"""
 __email__ = "camilo.velezramirez@nist.gov"
 __version__ = "0.1.0"
 
-__all__ = ["distance"]
+__all__ = ["archive",
+           "base",
+           "classification",
+           "clustering",
+           "distance"
+           "instruments",
+           "pipelines",
+           "schemas",
+           "similarity",
+           "utils"]
 # __all__ = [
 #     "BaseDistance",
 #     "EuclideanDistance",
