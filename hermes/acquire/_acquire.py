e3c8daa87bc74f7ab67ff7b2cfc224c468991de7
Austin McDannald <austin.mcdannald@nist.gov>
2023-06-05 10:03:55 -0400

    populateing acquistion functions

--- hermes/acquire/_acquire.py
+++ hermes/acquire/_acquire.py
@@ -6,6 +6,8 @@ from pathlib import Path
 from typing import Optional
 
 import numpy as np
+import tensorflow as tf
+from scipy.special import erf
 
 @dataclass
 class Acquisition:
@@ -41,7 +43,7 @@ class UpperConfidenceBound(Acquisition):
     num_sigmas = 1.96
 
     def calculate(self):
-        next = np.argmax(self.mean + num_sigmas*np.sqrt(self.var))
+        next = np.argmax(self.mean + self.num_sigmas*np.sqrt(self.var))
         return next
     
 @dataclass
@@ -51,14 +53,66 @@ class ScheduledUpperConfidenceBound(Acquisition):
     num_sigmas = 1.96
     
     def calculate(self):
-        beta = num_sigmas*num_measurements/2
+        beta = self.num_sigmas*self.num_measurements/2
         next = np.argmax(self.mean + beta*np.sqrt(self.var))
+        return next
 
 @dataclass
 class ThompsonSampling(Acquisition):
 
-    unmeasured_locations: np.array
-    full_cov: np.array
+    full_cov: tf.Tensor
+    batch_size: int
 
     def calculate(self):
-        # cov = 
+        #Sum full covariance to get NxN matrix
+        cov = tf.reduce_sum(self.full_cov, axis = 0)
+        
+        #Define a multivariate normal distribution and take draws from that
+        Y = np.random.multivariate_normal(np.array(self.mean).flatten(),
+                                  np.array(cov),
+                                  size = self.batch_size)
+  
+        #Find the max point in each draw
+        next_points = []
+        for y in Y:
+            next = np.argmax(y)
+            next_points.append(next)
+        
+        next_args = np.array(next_points)
+        
+        #Return the list of indexs and the draws
+        return next_args
+    
+@dataclass
+class ProbabilityofImprovement(Acquisition):
+    measurements: np.array
+
+    def calculate(self):
+        y_best = np.max(self.measurements)
+        #Calculate the probability of finding a measurment above the current best
+        PoI = 1 - 0.5*(1 + erf((y_best - self.mean)/(np.sqrt(self.var * 2))))
+
+
+        #Choose the highest probability
+        next = np.argmax(PoI)
+        return next
+
+@dataclass
+class ExpectedImprovement(Acquisition):
+    measurements: np.array
+
+    def calculate(self):
+        y_best = np.max(self.measurements)
+  
+        # alpha = (y_best - mean)/np.sqrt(var)
+        alpha = (self.mean - y_best)/np.sqrt(self.var)
+
+        phi_alpha = np.exp(-0.5*(alpha - 0)**2/1)/(np.sqrt(1*2*np.pi))
+        Phi_alpha = 0.5*(1 + erf((alpha - 0)/(np.sqrt(1 * 2))))
+
+        #Calculate the expected improvement
+        EI = (self.mean - y_best)*(Phi_alpha) + np.sqrt(self.var)*phi_alpha
+
+        #Choose the highest expected improvement
+        next = np.argmax(EI)
+        return next
